2cf235ba82015dafe5453e56aebc4ce1ca60114e
OleksandrK-dev <ukorkushko@gmail.com>
2022-08-28 13:20:49 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <div>
+                <img
+                    src="https://play-lh.googleusercontent.com/DTzWtkxfnKwFO3ruybY1SKjJQnLYeuK3KmQmwV5OQ3dULr5iXxeEtzBLceultrKTIUTr"/>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
